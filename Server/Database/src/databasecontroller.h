9b090200409bee9d0c554a3eed98cee2b7badc51
buglalis <bortnik.sm@phystech.edu>
2024-04-13 13:39:07 +0300

    Add two folders

--- Server/Database/src/databasecontroller.h
+++ Server/Database/src/databasecontroller.h
@@ -1,11 +1,6 @@
 #ifndef DATABASECONTROLLER_H
 #define DATABASECONTROLLER_H
-#include <QMainWindow>
-#include <QTableWidget>
-#include <QDialog>
 #include <QDebug>
-#include <QGridLayout>
-#include <QMainWindow>
 #include <QSqlTableModel>
 #include <database.h>
 
