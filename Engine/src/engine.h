2cd9452375290dad58ac560210b0c1a55e563a2f
Maksim Ermoshin <@po4ta>
2024-04-13 13:19:18 +0300

    Добавил систему вложенных cmake

---
+++ Engine/src/engine.h
@@ -0,0 +1,11 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <QDebug>
+#include <vector>
+#include "../../database/src/database.h"
+
+#pragma once
+
+
+#endif
---
+++ database/src/database.h
@@ -0,0 +1,12 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
+#include <QtSql>
+#include <QVector>
+
+
+
+#endif // DATABASE_H
